c63c98b6cb157eccd358431ebb468e36a4903e1c
zahir2000 <zakisher@gmail.com>
2019-11-17 20:15:19 +0800

    Landing page design

--- app/src/main/java/com/taruc/visory/MainActivity.kt
+++ app/src/main/java/com/taruc/visory/MainActivity.kt
@@ -2,11 +2,18 @@ package com.taruc.visory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    lateinit var sab: ActionBar
+
+    fun setActionBarTitle(title: String) {
+        sab.title = title
     }
 }
