d2f2973f83ac79e144d318dd7a3389bee7c799de
zahir2000 <zakisher@gmail.com>
2019-11-17 18:04:19 +0800

    added fragments and nav structure

---
+++ app/src/main/java/com/taruc/visory/volunteer/VolunteerForgotPassFragment.kt
@@ -0,0 +1,26 @@
+package com.taruc.visory.volunteer
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.taruc.visory.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class VolunteerForgotPassFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_volunteer_forgot_pass, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/taruc/visory/volunteer/VolunteerLoginFragment.kt
@@ -0,0 +1,22 @@
+package com.taruc.visory.volunteer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.taruc.visory.R
+
+class VolunteerLoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_volunteer_login, container, false)
+    }
+
+
+}
