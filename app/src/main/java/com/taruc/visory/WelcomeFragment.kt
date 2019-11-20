057591edf3490a1e1df882c0384b597d6f9d5592
zahir2000 <zakisher@gmail.com>
2019-11-20 19:17:06 +0800

    Some fixes

--- app/src/main/java/com/taruc/visory/WelcomeFragment.kt
+++ app/src/main/java/com/taruc/visory/WelcomeFragment.kt
@@ -8,8 +8,9 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
-class WelcomeFragment : Fragment() {
+class WelcomeFragment : Fragment(), View.OnClickListener {
 
     lateinit var navController: NavController
 
@@ -25,6 +26,17 @@ class WelcomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
         (activity as AppCompatActivity).supportActionBar?.title = "Welcome"
+
+        button_welcome_submit.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.button_welcome_submit -> {
+                // TODO: send to appropriate home screens
+                navController.navigate(R.id.action_welcomeFragment_to_blindHomeFragment)
+            }
+        }
     }
 
     // TODO: Disable the back button
--- app/src/main/java/com/taruc/visory/blind/BlindHomeFragment.kt
+++ app/src/main/java/com/taruc/visory/blind/BlindHomeFragment.kt
@@ -1,18 +1,19 @@
 package com.taruc.visory.blind
 
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.taruc.visory.R
 
-/**
- * A simple [Fragment] subclass.
- */
 class BlindHomeFragment : Fragment() {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +21,13 @@ class BlindHomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blind_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+        (activity as AppCompatActivity).supportActionBar?.title = "Home"
     }
 
 
