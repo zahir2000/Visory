ad4490aac81207c9a713ba5e0a1bab0f83ecedbc
zahir2000 <zakisher@gmail.com>
2019-12-03 00:38:56 +0800

    Login activity

--- app/src/main/java/com/taruc/visory/ui/LoginActivity.kt
+++ app/src/main/java/com/taruc/visory/ui/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.taruc.visory.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.taruc.visory.BlindHomeActivity
 import com.taruc.visory.R
+import com.taruc.visory.VolunteerHomeActivity
+import com.taruc.visory.utils.LoggedUserType
 import com.taruc.visory.utils.UserType
+import kotlinx.android.synthetic.main.activity_login.*
 import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
@@ -31,7 +41,69 @@ class LoginActivity : AppCompatActivity() {
 
         setTitle(R.string.label_login)
 
+        login_button_submit.setOnClickListener{
+            val email = email_text.text.toString()
+            val password = password_text.text.toString()
+            val loggedUserTypePref = LoggedUserType(this)
 
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(applicationContext, "Login Successful", Toast.LENGTH_SHORT).show()
+                        val user = auth.currentUser
+
+                        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+                        val rootRef = FirebaseDatabase.getInstance().getReference("users")
+                        val uidRef = rootRef.child(String.format("%s/role", uid))
+                        val valueEventListener = object : ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                loggedUserTypePref.setUserType(Integer.parseInt(dataSnapshot.getValue()!!.toString()))
+                            }
+                            override fun onCancelled(databaseError: DatabaseError) {
+                            }
+                        }
+                        uidRef.addListenerForSingleValueEvent(valueEventListener)
+
+                        Handler().postDelayed({
+                            try {
+                                if(auth.currentUser != null){
+                                    if(auth.currentUser!!.isEmailVerified){
+                                        /*if(loggedUserTypePref.getUserType() == 1){
+                                            val intent = Intent(this, VolunteerHomeActivity::class.java)
+                                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                            startActivity(intent)
+                                            finish()
+                                        }
+                                        else{
+                                            val intent = Intent(this, BlindHomeActivity::class.java)
+                                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                            startActivity(intent)
+                                            finish()
+                                        }*/
+
+                                        val intent = Intent(this, WelcomeActivity::class.java)
+                                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        startActivity(intent)
+                                        finish()
+                                    }
+                                    else{
+                                        val intent = Intent(this, VerifyEmailActivity::class.java)
+                                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                        startActivity(intent)
+                                        finish()
+                                    }
+                                }
+                            } catch (e: Exception) {}
+                        }, 3000)
+
+                        // get user -> check for role ->
+                } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(applicationContext, "Login Failed", Toast.LENGTH_SHORT).show()
+                    }
+        }
+    }
     }
 
     public override fun onStart() {
