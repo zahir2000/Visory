d369133acb0885ae8868eac502e76facbe3589e5
janksion <janksion100@gmail.com>
2020-01-08 18:42:24 +0800

    JC Intial Commit

---
+++ app/src/main/java/com/taruc/visory/Donation/DonationPayment.kt
@@ -0,0 +1,13 @@
+package com.taruc.visory.Donation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.taruc.visory.R
+
+class DonationPayment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_donation_payment)
+    }
+}
