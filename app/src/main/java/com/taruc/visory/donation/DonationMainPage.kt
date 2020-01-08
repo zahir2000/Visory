71a7fed104895884a1719630c97b572010d142ec
Janksion <qqq_1999@live.com.my>
2020-01-08 21:44:11 +0800

    JC Initial Commit

--- app/src/main/java/com/taruc/visory/donation/DonationMainPage.kt
+++ app/src/main/java/com/taruc/visory/donation/DonationMainPage.kt
@@ -1,31 +1,47 @@
 package com.taruc.visory.donation
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.fragment.app.Fragment
+import com.taruc.visory.MainActivity
 import com.taruc.visory.R
+import kotlinx.android.synthetic.main.fragment_donation_main_page.*
 
-class DonationMainPage : Fragment() {
+
+class DonationMainPage : Fragment(), View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.btnDonate -> {
+                activity?.let{
+                    val intent = Intent (it, DonationPayment::class.java)
+                    it.startActivity(intent)
+                }
+            }
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donation_main_page, container, false)
+        return inflater.inflate(com.taruc.visory.R.layout.fragment_donation_main_page, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (activity as AppCompatActivity).supportActionBar?.title = "Donation"
+        btnDonate.setOnClickListener(this)
     }
 }
