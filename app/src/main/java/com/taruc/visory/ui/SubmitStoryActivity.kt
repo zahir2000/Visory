38dc8f143b3f9661c6ff40b2f60342f9a62120ef
Jaloliddin Rustamov <mr.romaboy@gmail.com>
2020-01-09 08:56:05 +0800

    idk

--- app/src/main/java/com/taruc/visory/ui/SubmitStoryActivity.kt
+++ app/src/main/java/com/taruc/visory/ui/SubmitStoryActivity.kt
@@ -1,17 +1,24 @@
 package com.taruc.visory.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import com.taruc.visory.R
 import com.taruc.visory.utils.Story
 import com.taruc.visory.utils.getCurrentDate
 import com.taruc.visory.utils.shortToast
 import kotlinx.android.synthetic.main.activity_submit_story.*
+import java.util.*
 
 class SubmitStoryActivity : AppCompatActivity() {
 
@@ -30,30 +37,65 @@ class SubmitStoryActivity : AppCompatActivity() {
         textDate = findViewById(R.id.textDate)
         textDate.text = getCurrentDate()
 
+        btnSelectPhoto.setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+
+        }
         btnSubmit.setOnClickListener {
-            saveStory()
+            uploadImageToFirebaseStorage()
         }
 
     }
 
-    private fun saveStory(){
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,selectedPhotoUri)
+            imgStoryCover.setImageBitmap(bitmap)
+            btnSelectPhoto.alpha = 0f
+//            val bitmapDrawable = BitmapDrawable(bitmap)
+//            btnSelectPhoto.setBackgroundDrawable(bitmapDrawable)
+        }
+    }
+
+    private fun saveStory(storyCoverUrl:String){
         val title = editTextTitle.text.toString().trim()
         val story = editTextStory.text.toString()
+        val date = getCurrentDate()
+        val uid = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance().getReference("/stories/$uid")
 
+        val storyObj = Story(uid, storyCoverUrl, title, story, date)
         if(title.isEmpty()){
-            editTextTitle.error = "Please enter a title for the story!"
-            return
+           editTextTitle.error = "Please enter a title for the story!"
+           return
         }
 
-        val ref = FirebaseDatabase.getInstance().getReference("stories")
-        val id= ref.push().key
-        val date = getCurrentDate()
-        val storyObj = Story(id, title, story, date)
-
-        ref.child(id!!).setValue(storyObj).addOnCompleteListener{task ->
+        ref.setValue(storyObj).addOnCompleteListener{task ->
             if (task.isSuccessful){
                 shortToast("Story has been created!")
             }
         }
+    }
+
+    private fun uploadImageToFirebaseStorage(){
+
+        if(selectedPhotoUri==null)return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                ref.downloadUrl.addOnSuccessListener {
+                    saveStory(it.toString())
+                }
+            }
     }
 }
--- app/src/main/java/com/taruc/visory/utils/Story.kt
+++ app/src/main/java/com/taruc/visory/utils/Story.kt
@@ -1,3 +1,3 @@
 package com.taruc.visory.utils
 
-class Story(val story_id: String?, val sTitle: String, val sBody: String, val sDate: String)
+class Story(val uid: String?, val imageUrl: String,  val sTitle: String, val sBody: String, val sDate: String)
