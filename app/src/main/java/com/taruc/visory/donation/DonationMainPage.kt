fcae8f601de5ff2bd512035a041ed3ebf65c6172
janksion <qqq_1999@live.com.my>
2020-01-14 23:40:17 +0800

    JC Commit

--- app/src/main/java/com/taruc/visory/donation/DonationMainPage.kt
+++ app/src/main/java/com/taruc/visory/donation/DonationMainPage.kt
@@ -3,14 +3,8 @@ package com.taruc.visory.donation
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.taruc.visory.R
 import kotlinx.android.synthetic.main.fragment_donation_main_page.*
 
@@ -59,22 +53,21 @@ class DonationMainPage : Fragment(), View.OnClickListener {
                     val intent = Intent(it, DonateHistory::class.java)
                     it.startActivity(intent)
                     it.overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
-                    Toast.makeText(it,.toString(amount), Toast.LENGTH_LONG).show()
                 }
             }
         }
         return super.onOptionsItemSelected(item)
     }
 
-    val uid = FirebaseAuth.getInstance().currentUser!!.uid
-    val rootRef = FirebaseDatabase.getInstance().getReference("DonateDatabase")
-    var amount: Double = 0.00;
-    val valueEventListener = object : ValueEventListener {
-        override fun onDataChange(dataSnapshot: DataSnapshot) {
-            amount = (dataSnapshot.child("amount").value.toString())
-        }
-
-        override fun onCancelled(databaseError: DatabaseError) {
-        }
-    }
+//    val uid = FirebaseAuth.getInstance().currentUser!!.uid
+//    val rootRef = FirebaseDatabase.getInstance().getReference("DonateDatabase")
+//    var amount: Double = 0.00;
+//    val valueEventListener = object : ValueEventListener {
+//        override fun onDataChange(dataSnapshot: DataSnapshot) {
+//            amount = (dataSnapshot.child("amount").value.toString())
+//        }
+//
+//        override fun onCancelled(databaseError: DatabaseError) {
+//        }
+//    }
 }
