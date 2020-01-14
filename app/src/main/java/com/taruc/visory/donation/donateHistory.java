34edae554c2175bb3af8741997c21220a2c07f6e
janksion <qqq_1999@live.com.my>
2020-01-15 06:52:04 +0800

    JC Commit

--- app/src/main/java/com/taruc/visory/donation/donateHistory.java
+++ app/src/main/java/com/taruc/visory/donation/donateHistory.java
@@ -1,6 +1,7 @@
 package com.taruc.visory.donation;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,6 +21,7 @@ public class donateHistory extends AppCompatActivity {
         new FirebaseDatabaseHelper().readDonateHistory(new FirebaseDatabaseHelper.DataStatus() {
             @Override
             public void DataIsLoaded(List<DonateDatabase> donateDatabaseList, List<String> keys) {
+                findViewById(R.id.loadingProgressBar).setVisibility(View.GONE);
                 new RecyclerView_Config().setConfig(mRecycleView, donateHistory.this,donateDatabaseList,keys);
             }
         });
